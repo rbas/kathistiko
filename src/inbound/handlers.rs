b44f1224fe9eef1340512521a0508f208757409e
Martin Voldřich <rbas.cz@gmail.com>
2025-03-15 22:41:45 +0100

    chore: added html template to the handler

--- src/inbound/handlers.rs
+++ src/inbound/handlers.rs
@@ -1,7 +1,36 @@
-use axum::extract::State;
+use askama::Template;
+use axum::{
+    extract::State,
+    response::{Html, IntoResponse, Response},
+};
+use reqwest::StatusCode;
 
 use super::http::AppState;
 
-pub async fn calendar_handler(State(state): State<AppState>) -> &'static str {
-    "foo"
+pub struct HtmlTemplate<T>(pub T);
+
+impl<T> IntoResponse for HtmlTemplate<T>
+where
+    T: Template,
+{
+    fn into_response(self) -> Response {
+        match self.0.render() {
+            Ok(html) => Html(html).into_response(),
+            Err(err) => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Failed to render template. Error: {err}"),
+            )
+                .into_response(),
+        }
+    }
+}
+
+#[derive(Template)]
+#[template(path = "calendar.html")]
+pub struct CalendarTemplate {}
+
+pub async fn calendar_handler(State(state): State<AppState>) -> impl IntoResponse {
+    let template = CalendarTemplate {};
+
+    HtmlTemplate(template)
 }
