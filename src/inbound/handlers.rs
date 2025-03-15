1260dee439e5b99b23f44418ddb367590bd09276
Martin Voldřich <rbas.cz@gmail.com>
2025-03-15 23:44:44 +0100

    chore: Rendering all calendar data

--- src/inbound/handlers.rs
+++ src/inbound/handlers.rs
@@ -3,7 +3,14 @@ use axum::{
     extract::State,
     response::{Html, IntoResponse, Response},
 };
+use chrono::{DateTime, Datelike, Days, Local};
+use log::error;
 use reqwest::StatusCode;
+
+use crate::calendar::{
+    apple_calendar::{get_calendar_items, Item},
+    trash_events::{generate_periodical_events, PeriodicalItem},
+};
 
 use super::http::AppState;
 
@@ -27,10 +34,42 @@ where
 
 #[derive(Template)]
 #[template(path = "calendar.html")]
-pub struct CalendarTemplate {}
+pub struct CalendarTemplate {
+    pub current_date: DateTime<Local>,
+    pub periodical_items: Option<Vec<PeriodicalItem>>,
+    pub calendar_items: Option<Vec<Item>>,
+}
+
+impl CalendarTemplate {
+    pub fn get_month_name(&self) -> String {
+        format!("{}", self.current_date.format("%B"))
+    }
+}
 
 pub async fn calendar_handler(State(state): State<AppState>) -> impl IntoResponse {
-    let template = CalendarTemplate {};
+    let today = Local::now();
+
+    let date = today.date_naive();
+
+    let periodical_items = generate_periodical_events(date);
+
+    let url = state.settings.family_calendar_url.as_str();
+    let offset = Days::new(state.settings.family_calendar_offset_days);
+    let result = get_calendar_items(url, date, offset).await;
+
+    let calendar_items = match result {
+        Ok(items) => Some(items),
+        Err(err) => {
+            error!("Cannot read data from family calendar {:#?}", err);
+            None
+        }
+    };
+
+    let template = CalendarTemplate {
+        current_date: today,
+        periodical_items,
+        calendar_items,
+    };
 
     HtmlTemplate(template)
 }
